c554f4af3ae6bf9a117f594e69a1dcef64ed28a6
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2019-08-13 21:04:02 -0700

    Update indexfunds .rb

--- _funds/indexfunds .rb
+++ _funds/indexfunds .rb
@@ -4,8 +4,11 @@ voo
 
 =========Long term good etf 
 VTI Vanguard Total Stock Market  ,
-VT Vanguard World Stock Fund  ,
+VT  Vanguard World Stock Fund  ,
 VIG  Vanguard Dividend Appreciation Index Fund ETF Shares 
 
+VTSAX - Vanguard Total Stock Market Index Fund Inst .04% expense
+FZROX - Fidelity ZERO Total Market Index Fund  0%  expense mutual fund
+SWTSX - Schwab Total Stock Market Index Fund  .03%  expense, 
 
 =======
