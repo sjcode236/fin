601608d88dbac306a9930ab201c60268d358d770
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2020-02-04 21:16:20 -0700

    Create merrill-notes.rb

---
+++ follow/merrill-notes.rb
@@ -0,0 +1,27 @@
+
+support  877.653.4732
+ 
+To buy mutual funds without recurring buy charge 
+Trade => Mutual Funds => automatic investment plan => Eligible mutual funds include Merrill Edge Select™ Funds.
+
+Research => Market Calendedar => Major economic indicators 
+	=Gross domestic product
+	=Consumer price index
+	=Factory orders 
+Research => Market Calendedar => 	sectors & industries
+
+Research => Investing Insights => Get our perspective
+
+Research => stocks => News & Research => Third Party Opinions => CFRA Rankings Changes
+
+Research => Insights from BofA Global Research =>  
+	=12-month price objective   
+	=earnings estimates  
+Research => Insights from BofA Global Research => Flagship reports
+	=US1 Report 
+	=The RIC Report 
+	=US Economic Weekly 
+	
+Research => Mutual Funds => Merrill Edge Select Funds
+Research => Merrill Edge Select ETFs 
+	
