428250814ceb906fabc232997604f2bdb5897ac9
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2020-02-05 11:30:17 -0700

    Update notes-fidelity.rb

--- follow/notes-fidelity.rb
+++ follow/notes-fidelity.rb
@@ -4,3 +4,5 @@
 Research Scorecards    
 https://eresearch.fidelity.com/eresearch/goto/scorecards.jhtml  
 
+Explore Research Firms
+https://research2.fidelity.com/fidelity/research/reports/release2/Research/ExploreResearchFirms.asp   
