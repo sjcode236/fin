33352c6213ab531f88b6577a81e3403c0dce561e
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2020-03-19 00:40:24 -0700

    Update FB-youtubePicks.rb

--- _stoks/FB-youtubePicks.rb
+++ _stoks/FB-youtubePicks.rb
@@ -1,30 +1,9 @@
 
 
 ====JJ Buckner
-PEP  Pepsi 
-V  VISA 
-BA boeing 
-LUV  southwest 
-CCL  carnival 
 
 
 
 
 
-===misc  user    
-Amazon - 15%
-Google - 15%
-Apple - 12%
-Netflix -10%
-3M - 5%
-AT&T - 5%
-Coca Cola - 5%
-Johnson & Johnson - 5%
-JP Morgan - 5%
-McDonalds - 5%
-Disney - 4%
-Microsoft - 4%
-Vanguard - 4%
-Berkshire - 2%
-Facebook - 2%
-Nike - 2%
+
