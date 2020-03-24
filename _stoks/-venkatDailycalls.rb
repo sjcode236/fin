91864d157e9b7ca3f51277c6c1182b6c31f6f4d8
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2020-03-23 21:27:19 -0700

    Create -venkatDailycalls.rb

---
+++ _stoks/-venkatDailycalls.rb
@@ -0,0 +1,19 @@
+
+
+
+Play entry above 10.25, stop below 10, target 15.40
+AMZN  long above 1920, target 160  , stop below 1900 
+BA  long near 103, target 120 , stop below 100
+NVDA Long above 213.50  target 221 and 232   stop below 212 
+TTD Long on samllback to 174 , Tar 184  , stop 172.50 
+TTD long above 187 , target 208  , stop 184.50
+twlo: long above 90.50, target 93.25, stop below 89.50 
+MDB  long above 127.50 , target 131 and 135 , stop 126 
+ZG Long on pullTo 29 ; target 34.50 , stop 28
+BL Long on pull to 48.85 , target 52.50 stop below 48.25 
+TNDM Long on 52.75 Targ 56.25  stop 51.50 
+
+
+
+
+
