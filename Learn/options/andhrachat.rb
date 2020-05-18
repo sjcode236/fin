736baed39f6ea95be4b2e542f4b7b1007ef7c717
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2020-05-18 00:49:07 -0700

    Create andhrachat.rb

---
+++ Learn/options/andhrachat.rb
@@ -0,0 +1,49 @@
+
+
+
+
+
+================================================================================================
+Options are risky 
+Always think of how much will I lose if I am wrong 
+
+Always play limited risk strategies 
+
+When you buy a call or a put your risk is the amount you invested
+
+When you sell a call or a put your risk is unlimited 
+
+Selling puts and collecting premium is very tempting and you can make consistent money 
+But you always have to hedge your position
+
+So do not SELL an option without a HEDGE
+
+With the hedge in plan you can sleep 
+
+In the case mentioned above
+Halliburton was selling at $23 
+
+He sold in the money put $25 strike and collected premium
+
+He should have bought an equal number of puts with strike price $20 expiring in the same month
+
+So his risk is now predetermined at $500 per contract 
+
+When HAL went to $5  he was losing on the puts he sold but winning on the puts he bought
+with risk predetermined to $500 .
+
+If you want to play the selling game try this strategy:
+
+On stocks that are in an uptrend sell far out of the money puts
+
+
+On stocks that are in downtrend sell far out of the money calls 
+
+So what you are selling is time value  or insurance to somebody
+
+With every passing day the time value is decreasing 
+My analogy is the option is like an ice cube that is constantly melting 
+
+But even in this strategy hedge so you can sleep well
+My two cents for what it is worth for all in the group
+==========================================================================================================
