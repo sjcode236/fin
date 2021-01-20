de3166948542f60e3666e71451b331dd297a311a
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2021-01-19 19:58:08 -0700

    Update etfToSaveList.py

--- _funds/etfToSaveList.py
+++ _funds/etfToSaveList.py
@@ -5,11 +5,6 @@
 
 ==========================
 
-• Retail ETF (NYSE:XRT) increased 0.77% to $30.25.   
-• Energy     (NYSE:XLE) rose 4.09% to $29.79.
-• Technology (NYSE:XLK) rose 0.99% to $82.73.
-• Financial  (NYSE:XLF) decreased 0.12% to $21.39.   
-• Healthcare ETF ( XLV )
 ========   
 IPO - RENISSANCE IPE ETF   
 
